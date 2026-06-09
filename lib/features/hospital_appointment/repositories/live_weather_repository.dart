6708c076b3ca54b2abe473586539d1ae293251b1
Vinoryz <brofistjr10@gmail.com>
2026-06-10 02:46:00 +0700

    draft week 3

--- lib/features/hospital_appointment/repositories/live_weather_repository.dart
+++ lib/features/hospital_appointment/repositories/live_weather_repository.dart
@@ -4,7 +4,7 @@ import 'weather_repository.dart';
 
 class LiveWeatherRepository implements WeatherRepository {
   // Placeholder API Key (to be configured in constants later)
-  final String _apiKey = 'YOUR_OPENWEATHERMAP_API_KEY';
+  final String _apiKey = '4ac38ecff93e440072b2b487e83f2e2e';
 
   @override
   Future<String> getWeatherForDate(String dateString) async {
@@ -13,14 +13,14 @@ class LiveWeatherRepository implements WeatherRepository {
       final url = Uri.parse(
         'https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=$_apiKey&units=metric',
       );
-      
+
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
         final temp = data['main']['temp'];
         final description = data['weather'][0]['description'] as String?;
-        
+
         final descFormatted = description != null && description.isNotEmpty
             ? "${description[0].toUpperCase()}${description.substring(1)}"
             : "Unknown";
